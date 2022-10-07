af8ce99984a5e1808899259372377b986fae6699
Ravijadu <rk20021999@gmail.com>
2022-10-08 01:38:57 +0530

    changes

--- btre/settings.py
+++ btre/settings.py
@@ -13,10 +13,17 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files
+    os.path.join(BASE_DIR,  'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
